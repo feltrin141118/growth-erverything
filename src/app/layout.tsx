8b255f419bac14ce95024fe0f8b2f072431c06ec
wellington_santos <feltrin141118@gmail.com>
2026-02-23 07:21:30 -0300

    UI: Remover header da tela de login e centralizar branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header'
+import HeaderWrapper from '@/components/HeaderWrapper'
 import './globals.css'
 
 export const metadata = {
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body>
-        <Header /> 
+        <HeaderWrapper />
         <main>{children}</main>
       </body>
     </html>
